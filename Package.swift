33a011c586e2cdcf240ca56685cf4442f9b43f38
genexusbot <builder@genexus.com>
2023-03-09 07:11:34 -0300

    Package version: 1.0.0-beta+20230309100338

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXDataLayerRemote",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXDataLayerRemote-1.0.0-beta+20230309092859.xcframework.zip",
-			checksum: "873c98c91b6865a6eac4e6bf15000368d2587604a07166252704ad03c2d25b8b"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXDataLayerRemote-1.0.0-beta+20230309100338.xcframework.zip",
+			checksum: "0dcf58ec41bb1d2fde0992e17e8b7c99bfa9d8f23f9ba1a0fccc15596590c303"
 		)
 	]
 )
