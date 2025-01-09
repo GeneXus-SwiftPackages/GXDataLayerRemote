b68bb90f26342f3132e7ba0f1f79b6e58718e556
The iOS Team @ GeneXus <builder@genexus.com>
2025-01-09 14:32:55 -0300

    Package version: 3.0.0-rc.2

--- Package.swift
+++ Package.swift
@@ -1,28 +1,28 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
 	name: "GXDataLayerRemote",
-	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	platforms: [.iOS("13.0"), .watchOS("9.0"), .tvOS("13.0"), .visionOS("1.0")],
 	products: [
 		.library(
 			name: "GXDataLayerRemote",
 			targets: ["GXDataLayerRemoteWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", exact: "3.0.0-rc.2")
 	],
 	targets: [
 		.target(name: "GXDataLayerRemoteWrapper",
 				dependencies: [
 					"GXDataLayerRemote",
-					.product(name: "GXDataLayer", package: "GXDataLayer", condition: .when(platforms: [.watchOS, .iOS, .tvOS]))
+					.product(name: "GXDataLayer", package: "GXDataLayer", condition: .when(platforms: [.iOS, .watchOS, .tvOS, .visionOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXDataLayerRemote",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXDataLayerRemote-1.1.0.xcframework.zip",
-			checksum: "f0a61674bab1c3938f4ead606b4b80f9105475ebe4df0c41fd85ec421ed9a23b"
+			url: "https://pkgs.genexus.dev/iOS/preview/GXDataLayerRemote-3.0.0-rc.2.xcframework.zip",
+			checksum: "a4f371bd59f533ee03aa48a9c514a13caec4a04b026241ad697a7f1f6efb0469"
 		)
 	]
 )
