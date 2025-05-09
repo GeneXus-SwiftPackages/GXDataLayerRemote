181a108d5ce936778e2a6f40ddccac86d70dd353
The iOS Team @ GeneXus <builder@genexus.com>
2025-05-08 23:21:25 -0300

    Package version: 3.1.0-beta.27

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXDataLayerRemoteWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", exact: "3.1.0-beta.26")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayer.git", exact: "3.1.0-beta.27")
 	],
 	targets: [
 		.target(name: "GXDataLayerRemoteWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXDataLayerRemote",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXDataLayerRemote-3.1.0-beta.26.xcframework.zip",
-			checksum: "569a68d48fa6b098134fde93cb28dea8b1c36c8fa5f6cd0da262590536265bef"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXDataLayerRemote-3.1.0-beta.27.xcframework.zip",
+			checksum: "ad7d000a39e124c7ca85d387aa2034e4eb113764279eca9317095cbda1103e49"
 		)
 	]
 )
